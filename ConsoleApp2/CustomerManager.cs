99f10667b729380efae8232e312e712bc5ed337c
İREM ÜSTÜN <16030411060@ybu.edu.tr>
2021-03-25 00:11:25 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerInfo
+{
+    class CustomerManager
+    {
+        public void Add(int Id, string Name, string Surname, int Age)
+        {
+            Console.WriteLine("Tebrikler, müşteri " + Id + " eklendi!");
+        }
+
+       
+        public void List(int Id, string Name, string Surname, int Age)
+        {
+            Console.WriteLine("Listeleme başarılı!");
+        }
+
+        public void Delete(int Id, string Name, string Surname, int Age)
+        {
+            Console.WriteLine("Tebrikler, müşteri " + Id + " silindi!");
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CustomerInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Ayşe";
+            customer1.Surname = "Yılmaz";
+            customer1.Age = 25;
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.Name = "Selim";
+            customer2.Surname = "Saygın";
+            customer2.Age = 38;
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.Name = "Tarık";
+            customer3.Surname = "Bozgun";
+            customer3.Age = 32;
+
+            Customer customer4 = new Customer();
+            customer4.Id = 4;
+            customer4.Name = "Ahmet";
+            customer4.Surname = "Demir";
+            customer4.Age = 21;
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine(customer.Id + ". " + customer.Name + " " + customer.Surname + " " + customer.Age);
+            }
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(3, "Tarık", "Bozgun", 32);
+            customerManager.List(4, "Ahmet", "Demir", 21);
+            customerManager.Delete(1, "Ayşe", "Yılmaz", 25);
+        }
+    }
+}
